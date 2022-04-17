a263286867207ddf3108ab4f91d8e9154c82c819
rkdals213 <rkdals213@naver.com>
2022-04-17 09:40:43 +0900

    db connection test

--- src/main/java/com/example/backend/TestController.java
+++ src/main/java/com/example/backend/TestController.java
@@ -3,11 +3,20 @@ package com.example.backend;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+
 @RestController
 public class TestController {
 
+    private final EntityManager entityManager;
+
+    public TestController(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @GetMapping("/heartbeat")
     public String heartbeat() {
+        entityManager.createQuery("select 1 from TestEntity t");
         return "OK";
     }
 }
---
+++ src/main/java/com/example/backend/TestEntity.java
@@ -0,0 +1,10 @@
+package com.example.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class TestEntity {
+    @Id
+    private Long id;
+}
